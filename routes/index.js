306d6ab1a4e3bfaa1e34d074800c37cf4118649e
Will D <wdemilio@gmail.com>
2016-03-21 12:27:31 +0000

    Unix date handling

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var dateParser = require('unix-date-parser/unix-date-parser.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get(/(^[0-9]+$)/, function(req, res, next) {
+router.get(/^[\/][\-0-9][0-9]+$/, function(req, res, next) {
+  
+  console.log(dateParser.dateParse(req.path.slice(1)));
   res.render('index', { title: 'Unix Date' });
 });
 
