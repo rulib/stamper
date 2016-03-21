1a7a4ca7e6e580ffcf47cde0045796183da8c52d
Will D <wdemilio@gmail.com>
2016-03-21 11:47:30 +0000

    Add numerical route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get(/(^[0-9]+$)/, function(req, res, next) {
+  res.render('index', { title: 'Unix Date' });
+});
+
 module.exports = router;
