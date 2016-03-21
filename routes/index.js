739b01fe3a5cdb585a295908b650b6c4fbb5f7cf
Will D <wdemilio@gmail.com>
2016-03-21 13:15:13 +0000

    Cleaning up and getting ready for Heroku

--- routes/index.js
+++ routes/index.js
@@ -8,17 +8,17 @@ router.get('/', function(req, res, next) {
 });
 
 router.get(/^[\/][\-0-9][0-9]+$/, function(req, res, next) {
-  
+  console.log("Handling as unix date " + req.path);
   res.json(dateParser.dateParse(req.path.slice(1)));
 });
 
 router.get(/^[\/][a-z]/, function(req, res, next) {
-  console.log(req.path)
+  console.log("Handling as natural date " + req.path);
   res.json(dateParser.naturalDateParse(req.path.slice(1)));
 });
 
-router.get(/^[\/][\-0-9]+[^0-9]+$/, function(req, res, next) {
-  
+router.get(/^[\/][\-0-9]+[^0-9]+$|^[\/][^\w]/, function(req, res, next) {
+  console.log("Handling as null date " + req.path);
   res.json({ "unix": "null", "natural": "null" });
 });
 
